821f1ce1efeda8dbe3278ac92419e57fd8e88320
tsubasayoshida <k021g1136@g.neec.ac.jp>
2023-01-23 14:38:02 +0900

    06-03.3Dモデルの分離_分離完了編

--- Model.cpp
+++ Model.cpp
@@ -1,35 +1,33 @@
-#include "Model.h"
-#include<cassert>
-#include<fstream>
-#include<sstream>
-#include<Windows.h>
 #include <DirectXTex.h>
-
+#include "Model.h"
+#include <cassert>
+#include <fstream>
+#include <sstream>
+#include <Windows.h>
 
 using namespace std;
-using namespace DirectX;
 
 //静的メンバ変数の実体
 ID3D12Device* Model::device = nullptr;
 
 Model* Model::LoadFromOBJ()
 {
-	//新たなModel型のインスタンスをnewする
+	//新たなModel型のインスタンスのメモリを確保
 	Model* model = new Model();
 
+	//デスクリプタヒープの生成
+	model->InitializeDescriptorHeap();
 
-	
-
-	model->LoadFromInternal();
+	//読み込み
+	model->LoadFromOBJInternal();
 
 	//バッファ生成
-
+	model->CreateBuffers();
 
 	return model;
-
 }
 
-void Model::LoadFromInternal()
+void Model::LoadFromOBJInternal()
 {
 	//ファイルストリーム
 	std::ifstream file;
@@ -130,5 +128,270 @@ void Model::LoadFromInternal()
 	}
 	//ファイルを閉じる
 	file.close();
+}
+
+void Model::LoadMaterial(const std::string& directoryPath, const std::string& filename)
+{
+	//ファイルストリーム
+	std::ifstream file;
+	//マテリアルファイルを開く
+	file.open(directoryPath + filename);
+	//ファイルオープン失敗をチェック
+	if (file.fail()) {
+		assert(0);
+	}
+	//1行ずつ読み込む
+	string line;
+	while (getline(file, line)) {
+		//1行分の文字列をストリームに変換
+		std::istringstream line_stream(line);
+		//半角スペース区切りで行の先頭文字列を取得
+		string key;
+		getline(line_stream, key, ' ');
+
+		//先頭のタブ文字は無視する
+		if (key[0] == '\t') {
+			key.erase(key.begin());//先頭の文字を削除
+		}
+		//先頭文字がnewmtlならマテリアル名
+		if (key == "newmtl") {
+			//マテリアル名読み込み
+			line_stream >> material.name;
+		}
+		//先頭文字がKaならアンビエント色
+		if (key == "Ka") {
+			line_stream >> material.ambient.x;
+			line_stream >> material.ambient.y;
+			line_stream >> material.ambient.z;
+		}
+		//先頭文字がKdならディフューズ色
+		if (key == "Kd") {
+			line_stream >> material.diffuse.x;
+			line_stream >> material.diffuse.y;
+			line_stream >> material.diffuse.z;
+		}
+		//先頭文字がKsならスペキュラー色
+		if (key == "Ks") {
+			line_stream >> material.specular.x;
+			line_stream >> material.specular.y;
+			line_stream >> material.specular.z;
+		}
+		//先頭文字がmap_Kdならテクスチャファイル名
+		if (key == "map_Kd") {
+			//テクスチャファイル名読み込み
+			line_stream >> material.textureFilename;
+			//テクスチャ読み込み
+			LoadTexture(directoryPath, material.textureFilename);
+		}
+	}
+	//ファイルを閉じる
+	file.close();
+}
+
+void Model::LoadTexture(const std::string& directoryPath, const std::string& filename)
+{
+	HRESULT result = S_FALSE;
+
+	TexMetadata metadata{};
+	ScratchImage scratchImg{};
+
+	//ファイルパスを結合
+	string filepath = directoryPath + filename;
+
+	//ユニコード文字列に変換する
+	wchar_t wfilepath[128];
+	int iBufferSize = MultiByteToWideChar(CP_ACP, 0, filepath.c_str(), -1, wfilepath, _countof(wfilepath));
+
+	// WICテクスチャのロード
+	//result = LoadFromWICFile( L"Resources/tex1.png", WIC_FLAGS_NONE, &metadata, scratchImg);
+	result = LoadFromWICFile(wfilepath, WIC_FLAGS_NONE, &metadata, scratchImg);
+	assert(SUCCEEDED(result));
+
+	ScratchImage mipChain{};
+	// ミップマップ生成
+	result = GenerateMipMaps(
+		scratchImg.GetImages(), scratchImg.GetImageCount(), scratchImg.GetMetadata(),
+		TEX_FILTER_DEFAULT, 0, mipChain);
+	if (SUCCEEDED(result)) {
+		scratchImg = std::move(mipChain);
+		metadata = scratchImg.GetMetadata();
+	}
+
+	// 読み込んだディフューズテクスチャをSRGBとして扱う
+	metadata.format = MakeSRGB(metadata.format);
+
+	// リソース設定
+	CD3DX12_RESOURCE_DESC texresDesc = CD3DX12_RESOURCE_DESC::Tex2D(
+		metadata.format, metadata.width, (UINT)metadata.height, (UINT16)metadata.arraySize,
+		(UINT16)metadata.mipLevels);
+
+	// ヒーププロパティ
+	CD3DX12_HEAP_PROPERTIES heapProps =
+		CD3DX12_HEAP_PROPERTIES(D3D12_CPU_PAGE_PROPERTY_WRITE_BACK, D3D12_MEMORY_POOL_L0);
+
+	// テクスチャ用バッファの生成
+	result = device->CreateCommittedResource(
+		&heapProps, D3D12_HEAP_FLAG_NONE, &texresDesc,
+		D3D12_RESOURCE_STATE_GENERIC_READ, // テクスチャ用指定
+		nullptr, IID_PPV_ARGS(&texbuff));
+	assert(SUCCEEDED(result));
+
+	// テクスチャバッファにデータ転送
+	for (size_t i = 0; i < metadata.mipLevels; i++) {
+		const Image* img = scratchImg.GetImage(i, 0, 0); // 生データ抽出
+		result = texbuff->WriteToSubresource(
+			(UINT)i,
+			nullptr,              // 全領域へコピー
+			img->pixels,          // 元データアドレス
+			(UINT)img->rowPitch,  // 1ラインサイズ
+			(UINT)img->slicePitch // 1枚サイズ
+		);
+		assert(SUCCEEDED(result));
+	}
+
+	// シェーダリソースビュー作成
+	cpuDescHandleSRV = CD3DX12_CPU_DESCRIPTOR_HANDLE(descHeap->GetCPUDescriptorHandleForHeapStart(), 0, descriptorHandleIncrementSize);
+	gpuDescHandleSRV = CD3DX12_GPU_DESCRIPTOR_HANDLE(descHeap->GetGPUDescriptorHandleForHeapStart(), 0, descriptorHandleIncrementSize);
+
+	D3D12_SHADER_RESOURCE_VIEW_DESC srvDesc{}; // 設定構造体
+	D3D12_RESOURCE_DESC resDesc = texbuff->GetDesc();
+
+	srvDesc.Format = resDesc.Format;
+	srvDesc.Shader4ComponentMapping = D3D12_DEFAULT_SHADER_4_COMPONENT_MAPPING;
+	srvDesc.ViewDimension = D3D12_SRV_DIMENSION_TEXTURE2D;//2Dテクスチャ
+	srvDesc.Texture2D.MipLevels = 1;
+
+	device->CreateShaderResourceView(texbuff.Get(), //ビューと関連付けるバッファ
+		&srvDesc, //テクスチャ設定情報
+		cpuDescHandleSRV
+	);
+
+}
+
+void Model::InitializeDescriptorHeap()
+{
+	HRESULT result = S_FALSE;
+
+	// デスクリプタヒープを生成	
+	D3D12_DESCRIPTOR_HEAP_DESC descHeapDesc = {};
+	descHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV;
+	descHeapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_SHADER_VISIBLE;//シェーダから見えるように
+	descHeapDesc.NumDescriptors = 1; // シェーダーリソースビュー1つ
+	result = device->CreateDescriptorHeap(&descHeapDesc, IID_PPV_ARGS(&descHeap));//生成
+	if (FAILED(result)) {
+		assert(0);
+	}
+
+	// デスクリプタサイズを取得
+	descriptorHandleIncrementSize = device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV);
+
+}
+
+void Model::CreateBuffers()
+{
+	HRESULT result = S_FALSE;
+
+	std::vector<VertexPosNormalUv> realVertices;
+
+
+	UINT sizeVB = static_cast<UINT>(sizeof(VertexPosNormalUv) * vertices.size());
+	UINT sizeIB = static_cast<UINT>(sizeof(unsigned short) * indices.size());
+
+	// ヒーププロパティ
+	CD3DX12_HEAP_PROPERTIES heapProps = CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_UPLOAD);
+	// リソース設定
+	CD3DX12_RESOURCE_DESC resourceDesc = CD3DX12_RESOURCE_DESC::Buffer(sizeVB);
+
+	// 頂点バッファ生成
+	result = device->CreateCommittedResource(
+		&heapProps, D3D12_HEAP_FLAG_NONE, &resourceDesc, D3D12_RESOURCE_STATE_GENERIC_READ, nullptr,
+		IID_PPV_ARGS(&vertBuff));
+	assert(SUCCEEDED(result));
+
+	// 頂点バッファへのデータ転送
+	VertexPosNormalUv* vertMap = nullptr;
+	result = vertBuff->Map(0, nullptr, (void**)&vertMap);
+	if (SUCCEEDED(result)) {
+		//memcpy(vertMap, vertices, sizeof(vertices));
+		std::copy(vertices.begin(), vertices.end(), vertMap);
+		vertBuff->Unmap(0, nullptr);
+	}
+
+	// 頂点バッファビューの作成
+	vbView.BufferLocation = vertBuff->GetGPUVirtualAddress();
+	//vbView.SizeInBytes = sizeof(vertices);
+	vbView.SizeInBytes = sizeVB;
+	vbView.StrideInBytes = sizeof(vertices[0]);
+
+	//UINT sizeIB = static_cast<UINT>(sizeof(indices));
+	// リソース設定
+	resourceDesc.Width = sizeIB;
+
+	resourceDesc = CD3DX12_RESOURCE_DESC::Buffer(sizeIB);
+	// インデックスバッファ生成
+	result = device->CreateCommittedResource(
+		&heapProps, D3D12_HEAP_FLAG_NONE, &resourceDesc, D3D12_RESOURCE_STATE_GENERIC_READ, nullptr,
+		IID_PPV_ARGS(&indexBuff));
+
+	// インデックスバッファへのデータ転送
+	unsigned short* indexMap = nullptr;
+	result = indexBuff->Map(0, nullptr, (void**)&indexMap);
+	if (SUCCEEDED(result)) {
+
+		// 全インデックスに対して
+		//for (int i = 0; i < _countof(indices); i++)
+		//{
+		//	indexMap[i] = indices[i];	// インデックスをコピー
+		//}
+		std::copy(indices.begin(), indices.end(), indexMap);
+
+		indexBuff->Unmap(0, nullptr);
+	}
+
+	// インデックスバッファビューの作成
+	ibView.BufferLocation = indexBuff->GetGPUVirtualAddress();
+	ibView.Format = DXGI_FORMAT_R16_UINT;
+	//ibView.SizeInBytes = sizeof(indices);
+	ibView.SizeInBytes = sizeIB;
+
+	// 定数バッファの生成
+	result = device->CreateCommittedResource(
+		&heapProps,
+		D3D12_HEAP_FLAG_NONE,
+		&resourceDesc1,
+		D3D12_RESOURCE_STATE_GENERIC_READ,
+		nullptr,
+		IID_PPV_ARGS(&constBuffB1));
+	assert(SUCCEEDED(result));
+
+	// 定数バッファへデータ転送
+	ConstBufferDataB1* constMap1 = nullptr;
+	result = constBuffB1->Map(0, nullptr, (void**)&constMap1);
+	constMap1->ambient = material.ambient;
+	constMap1->diffuse = material.diffuse;
+	constMap1->specular = material.specular;
+	constMap1->alpha = material.alpha;
+	constBuffB1->Unmap(0, nullptr);
+}
+
+void Model::Draw(ID3D12GraphicsCommandList* cmdList, UINT rootParamIndexMaterial)
+{
+
+
+	// 頂点バッファの設定
+	cmdList->IASetVertexBuffers(0, 1, &vbView);
+	// インデックスバッファの設定
+	cmdList->IASetIndexBuffer(&ibView);
+
+	cmdList->SetGraphicsRootConstantBufferView(rootParamIndexMaterial, constBuffB1->GetGPUVirtualAddress());
+
+	// デスクリプタヒープの配列
+	ID3D12DescriptorHeap* ppHeaps[] = { descHeap.Get() };
+	cmdList->SetDescriptorHeaps(_countof(ppHeaps), ppHeaps);
+
+	// シェーダリソースビューをセット
+	cmdList->SetGraphicsRootDescriptorTable(2, gpuDescHandleSRV);
+
+	cmdList->DrawIndexedInstanced((UINT)indices.size(), 1, 0, 0, 0);
 
 }
--- Model.h
+++ Model.h
@@ -6,59 +6,119 @@
 #include <vector>
 #include <d3dx12.h>
 
+using namespace DirectX;
+
+//3Dモデル
 class Model
 {
-public: //静的メンバ関数
-	//OBJファイルから3Dモデルを読み込む
-	static Model* LoadFromOBJ();
-
-	//setter
-	static void SetDevice(ID3D12Device* device) { Model::device = device; }
-
-public: //サブクラス
-	//頂点データ構造体
-	struct VertexPosNormalUv
-	{
-		DirectX::XMFLOAT3 pos; // xyz座標
-		DirectX::XMFLOAT3 normal; // 法線ベクトル
-		DirectX::XMFLOAT2 uv;  // uv座標
-	};
-
+public:
 	//マテリアル
 	struct Material
 	{
-		std::string name;   //マテリアル名
-		DirectX::XMFLOAT3 ambient;   //アンビエント影響度
-		DirectX::XMFLOAT3 diffuse;   //ディフューズ影響度
-		DirectX::XMFLOAT3 specular;  //スペキュラー影響度
-		float alpha;        //アルファ
-		std::string textureFilename;  //テクスチャファイル名
+		std::string name;//マテリアル名
+		XMFLOAT3 ambient;//アンビエント影響度
+		XMFLOAT3 diffuse;//ディフューズ影響度
+		XMFLOAT3 specular;//スペキュラー影響度
+		float alpha;//アルファ
+		std::string textureFilename;//テクスチャファイル名
 		//コンストラクタ
-		Material()
-		{
+		Material() {
 			ambient = { 0.3f,0.3f,0.3f };
 			diffuse = { 0.0f,0.0f,0.0f };
 			specular = { 0.0f,0.0f,0.0f };
 			alpha = 1.0f;
 		}
 	};
+	// 頂点データ構造体
+	struct VertexPosNormalUv
+	{
+		XMFLOAT3 pos; // xyz座標
+		XMFLOAT3 normal; // 法線ベクトル
+		XMFLOAT2 uv;  // uv座標
+	};
 
-private: //メンバ変数
-	//頂点インデックス
-	std::vector<unsigned short>indices;
+	// 定数バッファ用データ構造体B1
+	struct ConstBufferDataB1
+	{
+		XMFLOAT3 ambient;//アンビエント係数
+		float pad1;      //パディング
+		XMFLOAT3 diffuse;//ディフューズ係数
+		float pad2;      //パディング
+		XMFLOAT3 specular;//スペキュラー係数
+		float alpha;     //アルファ
+	};
 
-	//テクスチャ読み込み
-	bool LoadTexture(const std::string& directoryPath, const std::string& filename);
+public://静的メンバ関数
+	//OBJファイルから3Dモデルを読み込む
+	static Model* LoadFromOBJ();
 
+	/// <summary>
+	/// マテリアル読み込み
+	/// </summary>
+	void LoadMaterial(const std::string& directoryPath, const std::string& filename);
+
+	/// <summary>
+	/// テクスチャ読み込み
+	/// </summary>
+	void LoadTexture(const std::string& directoryPath, const std::string& filename);
+
+	/// <summary>
+	/// 描画
+	/// </summary>
+	void Draw(ID3D12GraphicsCommandList* cmdList,UINT rootParamIndexMaterial);
+
+	/// <summary>
+	/// デスクリプタヒープの初期化
+	/// </summary>
+	void InitializeDescriptorHeap();
+
+	/// <summary>
+	/// 各種バッファ生成
+	/// </summary>
+	void CreateBuffers();
+
+private://メンバ変数
+	// 頂点インデックス配列
+	std::vector<unsigned short> indices;
+
+	//マテリアル
+	Material material;
+
+	//頂点データ配列
+	std::vector<VertexPosNormalUv> vertices;
+
+	// テクスチャバッファ
+	Microsoft::WRL::ComPtr<ID3D12Resource> texbuff;
+
+	// シェーダリソースビューのハンドル(CPU)
+	CD3DX12_CPU_DESCRIPTOR_HANDLE cpuDescHandleSRV;
+	// シェーダリソースビューのハンドル(CPU)
+	CD3DX12_GPU_DESCRIPTOR_HANDLE gpuDescHandleSRV;
+	// デスクリプタヒープ
+	Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> descHeap;
+	// デスクリプタサイズ
+	UINT descriptorHandleIncrementSize;
+	// 頂点バッファ
+	Microsoft::WRL::ComPtr<ID3D12Resource> vertBuff;
+	// インデックスバッファ
+	Microsoft::WRL::ComPtr<ID3D12Resource> indexBuff;
+	// 頂点バッファビュー
+	D3D12_VERTEX_BUFFER_VIEW vbView;
+	// インデックスバッファビュー
+	D3D12_INDEX_BUFFER_VIEW ibView;
+	// 定数バッファ
+	Microsoft::WRL::ComPtr<ID3D12Resource> constBuffB1;
+
+	CD3DX12_RESOURCE_DESC resourceDesc1 = CD3DX12_RESOURCE_DESC::Buffer((sizeof(ConstBufferDataB1) + 0xff) & ~0xff);
 	
-
-private: //非公開のメンバ関数
-	//OBJファイルから3Dモデルを読み込む(非公開)
-	void LoadFromInternal();
-
-private:  //静的メンバ変数
+private://静的メンバ変数
 	//デバイス
 	static ID3D12Device* device;
 
+private://非公開のメンバ関数
+	//OBJファイルから3Dモデルを読み込む(非公開)
+	void LoadFromOBJInternal();
+public:
+	//setter
+	static void SetDevice(ID3D12Device* device) { Model::device = device; }
 };
-
